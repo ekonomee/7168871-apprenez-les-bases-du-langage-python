f69179c44407f28963645eb6767a93c5c2663351
ekonomee <reyan3rd@gmail.com>
2025-06-18 12:41:53 +0200

    main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+nom = nome
+prenom = eko
+age = 20
+
+print(f"Je m'appelle {prenom}{nom} et j'ai {age}")
+age + 10
+print(f"Je m'appelle {prenom}{nom} et j'ai {age} maintenant")
